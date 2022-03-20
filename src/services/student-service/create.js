6f00769cf5ee877ed6798de089063eb5e7cb87f6
Jose Maria CL <chema9703@gmail.com>
2022-03-20 03:05:32 -0600

    Add functions for student and instructor

---
+++ src/services/student-service/create.js
@@ -0,0 +1,13 @@
+import * as uid from "uuid";
+import { ROLES } from "../../permissions";
+import { LocalPersistanceService } from "../local-persistance-service";
+
+export const create = ({
+  name,
+  email
+}) => {
+  return LocalPersistanceService.saveData(
+    "users",
+    { name, email, id: uid.v4(), role: ROLES.STUDENT }
+  )
+}
