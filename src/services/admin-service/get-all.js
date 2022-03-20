2dad7126bf653cb6e103fa90ed028bc9de5db89b
Jose Maria CL <chema9703@gmail.com>
2022-03-20 02:11:20 -0600

    Add admin service

---
+++ src/services/admin-service/get-all.js
@@ -0,0 +1,7 @@
+import { ROLES } from "../../permissions";
+import { LocalPersistanceService } from "../local-persistance-service";
+
+export const getAll = async () => {
+  const users = await LocalPersistanceService.getData("users")
+  return users.filter(u => u.role === ROLES.ADMIN)
+};
--- src/services/user-service/index.js
+++ src/services/user-service/index.js
@@ -1,13 +1,3 @@
-import { create } from "./create";
-import { update } from "./update";
-import { getAll } from "./get-all";
-import { getById } from "./get-by-id";
-import { deleteById } from "./delete-by-id";
-
-export const UserService = {
-  create,
-  update,
-  getAll,
-  getById,
-  deleteById,
-}
+export * from "./delete-by-id"
+export * from "./get-by-id"
+export * from "./update"
